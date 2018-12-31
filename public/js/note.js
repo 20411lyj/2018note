c0971a9bf365bafcc2580b795e81909b385dca44
20411lyj <lpdt0079@naver.com>
2018-12-31 10:50:06 +0900

    첫번째커밋

---
+++ public/js/note.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBRigg8tGnU5PJFAdU6zr7d-a7r5QUiHgc",
+    authDomain: "noteapp-25eaf.firebaseapp.com",
+    databaseURL: "https://noteapp-25eaf.firebaseio.com",
+    projectId: "noteapp-25eaf",
+    storageBucket: "noteapp-25eaf.appspot.com",
+    messagingSenderId: "230121248511"
+  };
+  firebase.initializeApp(config);
+  </script>
